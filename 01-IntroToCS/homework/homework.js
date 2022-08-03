83955edf0d36448bb2d7034de0eb2846a9123b42
Joaquin Santiago Oliveira <104986417+fr0st1987@users.noreply.github.com>
2022-08-03 11:49:54 -0300

    Several resolutions to cases

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,78 +1,62 @@
 'use strict'
 
 function BinarioADecimal(num) {
-  // tu codigo aca
-/*it('should return 2', function() {
-    expect(BinarioADecimal('10')).toBe(2);
-  });
-  it('should return 7', function() {
-    expect(BinarioADecimal('111')).toBe(7);
-  });
-
-  // 111 y tiene que ser 7. */
-/* PRIMERA SOLUCION 
-
-
-let pos = 0
-let suma = 0
-for (let i = num.length -1; i >= 0; i --){
-  suma += Math.pow(2, pos)*num[i];
-  pos++
+ //Primera solución con método:
+return parseInt(num, 2)}
+/*Segunda solución con for:
+let sum = 0 // creo una variable para hacer la suma
+let pos = 0 // creo una variable para elevar la posición
+for (let i = num.length -1; i >= 0; i--) { //empiezo a recorrer el num dado por parametro, de derecha a izquierda
+  sum += Math.pow(2, pos) * num[i] // resuelvo que la suma, es la suma entre la elevación de 2 a la posición por el recorrido de la iteración
+  pos++ // voy cambiando la posición, que se que son 8.
 }
-return suma
-} */
+return sum} */
 
-/* SEGUNDA SOLUCION 
-let bin = []; dec = 0
-bin = num.split('').reverse();
-for (let i = 0; i <bin.length; i++) {
-  dec +=bin[i] * math.pow(2,i);
+/*Tercera solución dando vuelta directamente el num y recorriendo el array de izq a derecha.
+
+let array = [], sum = 0;
+array = num.split('').reverse();
+for (let i = 0; i < array.length; i++) {
+  sum += Math.pow(2, i) * array[i]
 }
-return dec;}*/
-
-
-
-return parseInt(num, 2) }
-
-function DecimalABinario(num) {
-  // tu codigo aca
-/*t('should return "100"', function() {
-    expect(DecimalABinario(4)).toBe('100');
-  });
-  it('should return "111"', function() {
-    expect(DecimalABinario(7)).toBe('111');
-  })*/
-
-  /* PRIMERA SOLUCION 
-let binario = '';
-let div = 0
-while (num / 2 !== 0) {
-  if (num % 2 == 0) {
-    binario += '0'}
-  else {
-    binario += '1'}
-}
-div = parseInt(num / 2);
-num = div;
-
-let nuevaCadena = ''
-for (let i =binario.length - 1; i >= 0; i--){
-  nuevaCadena += binario[i];
+return sum}*/
+/*Cuarta solución: 
+var decimal = 0, i = 0, resto;
+  
+  while (num != 0) {
+    resto = num % 10;
+    num = Number.parseInt(num / 10);
+    decimal += resto * Math.pow(2, i);
+    ++i;
   }
-return nuevaCadena
+return decimal
 }*/
 
-/*SEGUNDA SOLUCION 
+
+function DecimalABinario(num) {
+// primera opción con método://
+return num.toString(2)}
+/* segunda opción:
 let bin = []
 while (num > 0) {
-  bin.push (num % 2);
+  bin.unshift(num % 2);
+  num = Math.floor(num/2)}
+  return bin.join('')
+} */
+/*tercera opción: 
+let bin = []
+while (num > 0) {
+  bin.push(num % 2);
   num = Math.floor(num/2)}
   return bin.reverse().join('')
-}
-*/
-return num.toString(2)}
-
-
+}*/
+/*cuarta opción:
+let array = []
+while (num != 0) {
+  let residuo = num%2;
+  num = Math.floor(num/2)
+  array.unshift(residuo)
+}*/
 
 module.exports = {
   BinarioADecimal,
